b878036133c0a6fd3ff18a3586af205d4b5ef281
Tsubasa Tanabe <aburasoba.blog@gmail.com>
2022-01-16 14:36:09 +0900

    first commit

---
+++ app/Http/Controllers/SlackController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+
+class SlackController extends Controller
+{
+    public function receiveMessage(Request $request)
+    {
+        if ($request->input('type') == 'url_verification') {
+            return $request->input('challenge');
+        }
+
+        $input = $request->input();
+        Log::info($input['event']['text']); // 入力したテキスト
+        Log::info($input['event']['user']); // 入力したユーザーの ID
+        $isBot = $input['authorizations'][0]['is_bot'];
+        if ($isBot) {
+            Log::info('bot だよ');
+        } else {
+            Log::info('botじゃない');
+        }
+        return '';
+    }
+}
