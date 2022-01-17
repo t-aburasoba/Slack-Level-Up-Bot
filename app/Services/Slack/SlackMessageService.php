81f6fb2b83414a1e5cc88d8b873b2d8a7371d969
Tsubasa Tanabe <aburasoba.blog@gmail.com>
2022-01-17 11:59:22 +0900

    modify experience

--- app/Services/Slack/SlackMessageService.php
+++ app/Services/Slack/SlackMessageService.php
@@ -5,10 +5,13 @@ use Illuminate\Support\Facades\Log;
 
 class SlackMessageService
 {
+    const EXPERIENCE_MAGNIFICATION = 5;
     public function calculateExperience(array $event)
     {
         Log::info($event['text']); // 入力したテキスト
         Log::info($event['user']); // 入力したユーザーの ID
-        return mb_strlen($event['text']);
+        $textNumber = mb_strlen($event['text']);
+        $experience = floor($textNumber/self::EXPERIENCE_MAGNIFICATION);
+        return $experience;
     }
 }
