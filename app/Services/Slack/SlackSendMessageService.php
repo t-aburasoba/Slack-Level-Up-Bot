9959359bc27a3ebba0be713e5ecb801877c51f8f
Tsubasa Tanabe <aburasoba.blog@gmail.com>
2022-02-09 14:44:06 +0900

    add error log

--- app/Services/Slack/SlackSendMessageService.php
+++ app/Services/Slack/SlackSendMessageService.php
@@ -1,38 +1,45 @@
 <?php
 namespace App\Services\Slack;
 
+use Illuminate\Support\Facades\Log;
+
 class SlackSendMessageService
 {
     public function sendMessage(string $message)
     {
-        $headers = [
-            'Authorization: Bearer ' . config('app.slack-app-token'), //（1)
-            'Content-Type: application/json;charset=utf-8'
-        ];
-        
-        $url = "https://slack.com/api/chat.postMessage"; //(2)
-        
-        //(3)
-        $post_fields = [
-            "channel" => "#general",
-            "text" => $message,
-            "as_user" => true
-        ];
-        
-        $options = [
-            CURLOPT_URL => $url,
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_HTTPHEADER => $headers,
-            CURLOPT_POST => true,
-            CURLOPT_POSTFIELDS => json_encode($post_fields)
-        ];
-        
-        $ch = curl_init();
-        
-        curl_setopt_array($ch, $options);
-        
-        $result = curl_exec($ch);
-        
-        curl_close($ch);
+        try {
+            $headers = [
+                'Authorization: Bearer ' . config('app.slack-app-token'), //（1)
+                'Content-Type: application/json;charset=utf-8'
+            ];
+            
+            $url = "https://slack.com/api/chat.postMessage"; //(2)
+            
+            //(3)
+            $post_fields = [
+                "channel" => "#general",
+                "text" => $message,
+                "as_user" => true
+            ];
+            
+            $options = [
+                CURLOPT_URL => $url,
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_HTTPHEADER => $headers,
+                CURLOPT_POST => true,
+                CURLOPT_POSTFIELDS => json_encode($post_fields)
+            ];
+            
+            $ch = curl_init();
+            
+            curl_setopt_array($ch, $options);
+            
+            $result = curl_exec($ch);
+            
+            return curl_close($ch);
+        } catch (error $e) {
+            Log::error($e->getMessage());
+            return false;
+        }
     }
 }
