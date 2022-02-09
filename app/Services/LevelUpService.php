c0d09d39b9630d4e0df985896805ed045432ce02
Tsubasa Tanabe <aburasoba.blog@gmail.com>
2022-02-09 14:40:59 +0900

    remove log

--- app/Services/LevelUpService.php
+++ app/Services/LevelUpService.php
@@ -41,10 +41,7 @@ class LevelUpService
     public function levelUp(string $slackId, int $experience)
     {
         $user = $this->userRepository->getBySlackId($slackId);
-        Log::info('今回の経験値は：' . $experience);
-        Log::info('今までの経験値は：' . $user->total_experiences);
         $totalExperience = $user->total_experiences + $experience;
-        Log::info('経験値の合計は：' . $totalExperience);
         $level = $this->getLevel($totalExperience, $user);
         $data = [
             'total_experiences' => $totalExperience,
@@ -56,12 +53,9 @@ class LevelUpService
     public function getLevel($totalExperience, $user)
     {
         $nextLevel = $user->level + 1;
-        Log::info("次のレベルは" . $nextLevel);
         $levelsExperience = $this->levelsExperienceRepository->getByLevel($nextLevel);
         $nextTotalExperience = $levelsExperience->total_experiences;
-        Log::info('次のレベルアップに必要なのは' . $nextTotalExperience);
         if ($nextTotalExperience <= $totalExperience) {
-            Log::info('レベルアップをしました');
             $this->slackSendMessageService->sendMessage($user->slack_id . ' のレベルが ' . $nextLevel . ' にアップしました !!!');
             return $nextLevel;
         }
