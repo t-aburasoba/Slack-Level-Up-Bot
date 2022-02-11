a9a0a129a007c3fe96a952831a727c616e18851b
Tsubasa Tanabe <aburasoba.blog@gmail.com>
2022-02-11 21:13:45 +0900

    create lp content

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,9 +38,8 @@
                 <ul class="navbar-nav">
                     <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#about">About</a></li>
                     <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#how-to-use">How to use</a></li>
-                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#education">Education</a></li>
                     <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#skills">Skills</a></li>
-                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#interests">Interests</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#who-is-me">Who is me</a></li>
                     <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#thanks">Thanks</a></li>
                 </ul>
             </div>
@@ -57,14 +56,16 @@
                         Slack
                         <span class="text-primary">Level up bot</span>
                     </h1>
-                        @unless (session('message'))
-                            <div class="subheading mb-5">
-                                created by
-                                <a href="https://twitter.com/aburasobablog" target="_blank">@aburasoba</a>
-                            </div>
-                        @endunless
+                    <div class="subheading mb-5">
+                        created by
+                        <a href="https://twitter.com/aburasobablog" target="_blank">@aburasoba</a>
+                    </div>
                     <p class="lead mb-5">
-                        Slack を盛り上げるためにゲーミフィケーションを導入しましょう。</br>Slack Levelup Bot を導入すると各ユーザーの投稿やリアクションに応じてレベルが上がります。
+                        <span class="text-primary">現在 beta 版です。</span>
+                        <br>
+                        Slack を盛り上げるためにゲーミフィケーションを導入しましょう。
+                        </br>
+                        Slack Levelup Bot を導入すると各ユーザーの投稿やリアクションに応じてレベルが上がります。
                     </p>
                     <div class="mb-5">
                         <a href="https://slack.com/oauth/v2/authorize?client_id=771687717684.2967129835249&scope=chat:write,chat:write.customize,chat:write.public,commands,incoming-webhook&user_scope=channels:history,reactions:read"><img alt="Add to Slack" height="40" width="139" src="https://platform.slack-edge.com/img/add_to_slack.png" srcSet="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x" /></a>
@@ -86,59 +87,33 @@
                     <h2 class="mb-5">How to use</h2>
                     <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
                         <div class="flex-grow-1">
-                            <h3 class="mb-0">Senior Web Developer</h3>
-                            <div class="subheading mb-3">Intelitec Solutions</div>
-                            <p>Bring to the table win-win survival strategies to ensure proactive domination. At the end of the day, going forward, a new normal that has evolved from generation X is on the runway heading towards a streamlined cloud solution. User generated content in real-time will have multiple touchpoints for offshoring.</p>
+                            <h3 class="mb-3">Install to workspace</h3>
+                            <div>
+                                <a href="https://slack.com/oauth/v2/authorize?client_id=771687717684.2967129835249&scope=chat:write,chat:write.customize,chat:write.public,commands,incoming-webhook&user_scope=channels:history,reactions:read"><img alt="Add to Slack" height="40" width="139" src="https://platform.slack-edge.com/img/add_to_slack.png" srcSet="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x" /></a>
+                            </div>
                         </div>
-                        <div class="flex-shrink-0"><span class="text-primary">March 2013 - Present</span></div>
+                        <div class="flex-shrink-0"><span class="text-primary">STEP 1</span></div>
                     </div>
                     <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
                         <div class="flex-grow-1">
-                            <h3 class="mb-0">Web Developer</h3>
-                            <div class="subheading mb-3">Intelitec Solutions</div>
-                            <p>Capitalize on low hanging fruit to identify a ballpark value added activity to beta test. Override the digital divide with additional clickthroughs from DevOps. Nanotechnology immersion along the information highway will close the loop on focusing solely on the bottom line.</p>
+                            <h3 class="mb-3">Level Up</h3>
+                            <p>ワークスペースにインストールできたら、積極的に発言・反応を行いましょう。<br>積極的な活動を行うことで、あなたのレベルがどんどん上がっていきます。</p>
                         </div>
-                        <div class="flex-shrink-0"><span class="text-primary">December 2011 - March 2013</span></div>
+                        <div class="flex-shrink-0"><span class="text-primary">STEP 2</span></div>
                     </div>
                     <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
                         <div class="flex-grow-1">
-                            <h3 class="mb-0">Junior Web Designer</h3>
-                            <div class="subheading mb-3">Shout! Media Productions</div>
-                            <p>Podcasting operational change management inside of workflows to establish a framework. Taking seamless key performance indicators offline to maximise the long tail. Keeping your eye on the ball while performing a deep dive on the start-up mentality to derive convergence on cross-platform integration.</p>
+                            <h3 class="mb-3">Check Level</h3>
+                            <p>/level コマンドを使用して、あなたのレベル・次のレベルアップに必要な経験値を確認しましょう。</p>
                         </div>
-                        <div class="flex-shrink-0"><span class="text-primary">July 2010 - December 2011</span></div>
+                        <div class="flex-shrink-0"><span class="text-primary">STEP 3</span></div>
                     </div>
                     <div class="d-flex flex-column flex-md-row justify-content-between">
                         <div class="flex-grow-1">
-                            <h3 class="mb-0">Web Design Intern</h3>
-                            <div class="subheading mb-3">Shout! Media Productions</div>
-                            <p>Collaboratively administrate empowered markets via plug-and-play networks. Dynamically procrastinate B2C users after installed base benefits. Dramatically visualize customer directed convergence without revolutionary ROI.</p>
+                            <h3 class="mb-3">Coming soon ....</h3>
+                            <p>Slack での活動が活発になるような仕組みをどんどん開発していきます。ご要望等ございましたら <a href="https://twitter.com/aburasobablog" target="_blank">Twitter</a> からお問い合わせください。</p>
                         </div>
-                        <div class="flex-shrink-0"><span class="text-primary">September 2008 - June 2010</span></div>
-                    </div>
-                </div>
-            </section>
-            <hr class="m-0" />
-            <!-- Education-->
-            <section class="resume-section" id="education">
-                <div class="resume-section-content">
-                    <h2 class="mb-5">Education</h2>
-                    <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
-                        <div class="flex-grow-1">
-                            <h3 class="mb-0">University of Colorado Boulder</h3>
-                            <div class="subheading mb-3">Bachelor of Science</div>
-                            <div>Computer Science - Web Development Track</div>
-                            <p>GPA: 3.23</p>
-                        </div>
-                        <div class="flex-shrink-0"><span class="text-primary">August 2006 - May 2010</span></div>
-                    </div>
-                    <div class="d-flex flex-column flex-md-row justify-content-between">
-                        <div class="flex-grow-1">
-                            <h3 class="mb-0">James Buchanan High School</h3>
-                            <div class="subheading mb-3">Technology Magnet Program</div>
-                            <p>GPA: 3.56</p>
-                        </div>
-                        <div class="flex-shrink-0"><span class="text-primary">August 2002 - May 2006</span></div>
+                        <div class="flex-shrink-0"><span class="text-primary">STEP ?</span></div>
                     </div>
                 </div>
             </section>
@@ -153,38 +128,43 @@
                         <li class="list-inline-item"><i class="fab fa-laravel"></i></li>
                         <li class="list-inline-item"><i class="fab fa-html5"></i></li>
                         <li class="list-inline-item"><i class="fab fa-css3-alt"></i></li>
-                        <li class="list-inline-item"><i class="fab fa-js-squaAre"></i></li>
                         <li class="list-inline-item"><i class="fab fa-sass"></i></li>
                         <li class="list-inline-item"><i class="fab fa-npm"></i></li>
                     </ul>
-                    <div class="subheading mb-3">Workflow</div>
+                    <div class="subheading mb-3">Slack API</div>
                     <ul class="fa-ul mb-0">
                         <li>
                             <span class="fa-li"><i class="fas fa-check"></i></span>
-                            Mobile-First, Responsive Design
+                            Slack App
                         </li>
                         <li>
                             <span class="fa-li"><i class="fas fa-check"></i></span>
-                            Cross Browser Testing & Debugging
-                        </li>
-                        <li>
-                            <span class="fa-li"><i class="fas fa-check"></i></span>
-                            Cross Functional Teams
-                        </li>
-                        <li>
-                            <span class="fa-li"><i class="fas fa-check"></i></span>
-                            Agile Development & Scrum
+                            Incoming web hook
                         </li>
                     </ul>
                 </div>
             </section>
             <hr class="m-0" />
-            <!-- Interests-->
-            <section class="resume-section" id="interests">
+            <!-- Education-->
+            <section class="resume-section" id="who-is-me">
                 <div class="resume-section-content">
-                    <h2 class="mb-5">Interests</h2>
-                    <p>Apart from being a web developer, I enjoy most of my time being outdoors. In the winter, I am an avid skier and novice ice climber. During the warmer months here in Colorado, I enjoy mountain biking, free climbing, and kayaking.</p>
-                    <p class="mb-0">When forced indoors, I follow a number of sci-fi and fantasy genre movies and television shows, I am an aspiring chef, and I spend a large amount of my free time exploring the latest technology advancements in the front-end web development world.</p>
+                    <h2 class="mb-5">Who is me</h2>
+                    <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                        <div class="flex-grow-1">
+                            <h3 class="mb-0">Software Developer</h3>
+                            <div class="subheading mb-3">T.tsubasa</div>
+                            <div class="mb-3">Laravel engineer.</div>
+                            <div class="social-icons">
+                                <a class="social-icon" target="_blank" href="https://github.com/t-aburasoba">
+                                    <i class="fab fa-github"></i>
+                                </a>
+                                <a class="social-icon" target="_blank" href="https://twitter.com/aburasobablog">
+                                    <i class="fab fa-twitter"></i>
+                                </a>
+                            </div>
+                        </div>
+                        <div class="flex-shrink-0"><span class="text-primary">I like to play BCG</span></div>
+                    </div>
                 </div>
             </section>
             <hr class="m-0" />
@@ -195,41 +175,11 @@
                     <ul class="fa-ul mb-0">
                         <li>
                             <span class="fa-li"><i class="fas fa-trophy text-warning"></i></span>
-                            Google Analytics Certified Developer
+                            Slack での活動が活発になるような仕組みをどんどん開発していきます。ご要望等ございましたら <a href="https://twitter.com/aburasobablog" target="_blank">Twitter</a> からお問い合わせください。
                         </li>
                         <li>
                             <span class="fa-li"><i class="fas fa-trophy text-warning"></i></span>
-                            Mobile Web Specialist - Google Certification
-                        </li>
-                        <li>
-                            <span class="fa-li"><i class="fas fa-trophy text-warning"></i></span>
-                            1
-                            <sup>st</sup>
-                            Place - University of Colorado Boulder - Emerging Tech Competition 2009
-                        </li>
-                        <li>
-                            <span class="fa-li"><i class="fas fa-trophy text-warning"></i></span>
-                            1
-                            <sup>st</sup>
-                            Place - University of Colorado Boulder - Adobe Creative Jam 2008 (UI Design Category)
-                        </li>
-                        <li>
-                            <span class="fa-li"><i class="fas fa-trophy text-warning"></i></span>
-                            2
-                            <sup>nd</sup>
-                            Place - University of Colorado Boulder - Emerging Tech Competition 2008
-                        </li>
-                        <li>
-                            <span class="fa-li"><i class="fas fa-trophy text-warning"></i></span>
-                            1
-                            <sup>st</sup>
-                            Place - James Buchanan High School - Hackathon 2006
-                        </li>
-                        <li>
-                            <span class="fa-li"><i class="fas fa-trophy text-warning"></i></span>
-                            3
-                            <sup>rd</sup>
-                            Place - James Buchanan High School - Hackathon 2005
+                            アイデアをいただきました <a href="https://twitter.com/masagaogaoasia" target="_blank">Masagao</a> さんに感謝。
                         </li>
                     </ul>
                 </div>
