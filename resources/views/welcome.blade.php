b251a2565e54eaf77ec172b6ce23003c566d29e7
Tsubasa Tanabe <aburasoba.blog@gmail.com>
2022-02-11 18:29:00 +0900

    modify lp

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -25,7 +25,7 @@
         <!-- Navigation-->
         <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="sideNav">
             <a class="navbar-brand js-scroll-trigger" href="#page-top">
-                <span class="d-block d-lg-none">Slack Levelup Boyt</span>
+                <span class="d-block d-lg-none">Slack Levelup Bot</span>
                 <span class="d-none d-lg-block">
                     <img class="img-fluid img-profile rounded-circle mx-auto mb-2"
                          src="{{ asset('images/logo.png') }}"
